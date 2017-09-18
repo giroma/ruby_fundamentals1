be8bf904f20bd537b74264245e30d9aaa1f936c1
Argir Argirov <dj.giro@gmail.com>
2017-09-18 14:14:12 -0400

    exercise 2 done

---
+++ exercise2.rb
@@ -0,0 +1,19 @@
+
+bill=123
+tip= bill*0.1 #10 percent tip
+puts tip
+
+
+
+sum="hello".to_i + 34
+puts sum
+
+
+
+multiplication=45628 * 7839
+puts "the sum is #{multiplication}"
+
+
+#  true  &&  false   ||    true
+#       true
+(10 < 20 && 30 < 20) || !(10 == 11)
