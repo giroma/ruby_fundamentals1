a3d544f23e4d39ccb276e857a05f5e59e2dfe023
Argir Argirov <dj.giro@gmail.com>
2017-09-18 15:31:18 -0400

    DONE exercise5

---
+++ exercise5.rb
@@ -0,0 +1,12 @@
+distance = 0 #declare 0 value outside of the loop
+
+while true
+  puts "Do you want to walk or run?"
+  walk_or_run = gets.chomp
+  if walk_or_run == "walk" || walk_or_run == "w"
+    distance += 1
+  elsif walk_or_run == "run" || walk_or_run == "r"
+    distance += 5
+  end
+  puts "Distance from home is #{distance} km"
+end
