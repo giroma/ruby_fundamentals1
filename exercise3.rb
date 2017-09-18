68edcd57dd049e898894b98649bd1ce97fea1543
Argir Argirov <dj.giro@gmail.com>
2017-09-18 14:31:27 -0400

    COMPLETED age added

--- exercise3.rb
+++ exercise3.rb
@@ -1,8 +1,12 @@
-puts "What is your name?"
-user_name=gets
-puts "Hello #{user_name}"
+# puts "What is your name?"
+# user_name=gets
+# puts "Hello #{user_name}"
 
 
 puts "What is your name?"
 user_name=gets.chomp
 puts "Hello #{user_name}"
+
+puts "how old are you?"
+age=gets.to_i
+puts"you are #{age}"
